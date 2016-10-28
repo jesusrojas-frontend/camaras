09661a8bdedf07f73eb3fe77c75f77ccf8c0e7e6
Jesus rojas <Jesus rojas>
2016-10-28 13:02:59 -0400

    empezamos

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/estilos.css">
+	<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<nav class="navbar">
+		<div class="navbar-header">
+		    <button type="button" class="navbar-toggle collapsed fa fa-bars" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		    </button>
+		    <a class="navbar-brand" href="/diseno-y-desarrollo"><img src="/img/logo.png" alt="Logo"></a>
+
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+			        <li><a href="#"><i class="fa fa-user"></i> Cuenta</a></li>
+			        <li><a href="#"><i class="fa fa-shopping-cart"></i> Cart 0 Items(0.00 Bsf)</a></li>
+			        <li><a href="#"><i class="fa fa-search"></i></a></li>
+				</ul>
+			</div>
+	</nav>
